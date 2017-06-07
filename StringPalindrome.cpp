f60548e7fb9199c8978a19841de061bb7cb4d84c
lucacoppiardi <luca.coppiardi@apps.fermi.mn.it>
2017-06-07 12:11:07 +0200

    Pagina HTML (CSS) e grafico Gnuplot
    Il programma dopo aver eseguito il benchmark genera una pagina HTML-CSS con i risultati ed un grafico creato da Gnuplot

--- StringPalindrome.cpp
+++ StringPalindrome.cpp
@@ -4,6 +4,7 @@ using namespace std;
 #include <algorithm>
 #include <vector>
 #include <ctime>
+#include <fstream>
 
 #define BENCH_ITERATIONS 1000000
 
@@ -12,58 +13,59 @@ bool checkPalindrome_v2(const string& theString); // stringa iteratori inversi
 bool checkPalindrome_v3(const string& theString); // equal
 //bool checkPalindrome_exv3 (const string& theString); // equal non ottimizzato
 int doTest(const vector<string>& toCheck, bool(*checkPalindromeFunz)(const string&)); // benchmark
+void genera_html(int t1, int t2, int t3); // genera pagina html-css con risultati benchmark
+void genera_immagine_grafico (int t1, int t2, int t3); // genera grafico con gnuplot
 
 int main (int argc, char **argv) {
 	
-	/* // PROVA DI TUTTE LE FUNZIONI SU UNA SOLA STRINGA
-	string str;
-	cout << "Inserire stringa palindroma: ";
-	cin >> str;
-	cout << "Stringa inserita: " << str <<endl;
-	checkPalindrome_v1(str);
-	checkPalindrome_v2(str);
-	checkPalindrome_equals(str);
-	*/
+//	// PROVA DI TUTTE LE FUNZIONI SU UNA SOLA STRINGA
+//	string str;
+//	cout << "Inserire stringa palindroma: ";
+//	cin >> str;
+//	cout << "Stringa inserita: " << str <<endl;
+//	checkPalindrome_v1(str);
+//	checkPalindrome_v2(str);
+//	checkPalindrome_equals(str);
+	
 	vector<string> v1;
 	
-	v1.push_back("ciao");
-	v1.push_back("anna");
-	v1.push_back("inipotinonavevanotopi");
-	v1.push_back("itopinonavevanonipoti");
+	v1.push_back("ciao"); // corta
+	v1.push_back("anna"); // corta palindroma
+	v1.push_back("inipotinonavevanotopi"); // lunga
+	v1.push_back("itopinonavevanonipoti"); // lunga palindroma
 	
-	/* // PROVA DI TUTTE LE FUNZIONI SU UN VECTOR DI STRING
-	for (unsigned int i=0;i<v1.size(); i++) {
-		cout<<v1[i]<<endl;
-	}
+//	// PROVA DI TUTTE LE FUNZIONI SU UN VECTOR DI STRING
+//	for (unsigned int i=0;i<v1.size(); i++) {
+//		cout<<v1[i]<<endl;
+//	}
+//	
+//	cout<<endl<<"v1"<<endl;
+//	for (unsigned int i=0;i<v1.size();i++) {
+//		checkPalindrome_v1(v1[i]);
+//	}
+//	
+//	cout<<endl<<"v2"<<endl;
+//	for (unsigned int i=0;i<v1.size();i++) {
+//		checkPalindrome_v2(v1[i]);
+//	}
+//	
+//	cout<<endl<<"v3"<<endl;
+//	for (unsigned int i=0;i<v1.size();i++) {
+//		if (checkPalindrome_v3(v1[i]) == true) {
+//			cout << v1[i] << " e' palindroma" << endl;
+//		} else {
+//			cout << v1[i] << " non e' palindroma" << endl;
+//		}
+//	}
+//	
+//	cout<<endl;
 	
-	cout<<endl<<"v1"<<endl;
-	for (unsigned int i=0;i<v1.size();i++) {
-		checkPalindrome_v1(v1[i]);
-	}
+//	// BENCHMARK
+//	cout<<"Premere invio per iniziare il benchmark...";
+//	cin.get();
+//	cout<<"BENCH_ITERATIONS = "<<BENCH_ITERATIONS<<endl;
 	
-	cout<<endl<<"v2"<<endl;
-	for (unsigned int i=0;i<v1.size();i++) {
-		checkPalindrome_v2(v1[i]);
-	}
-	
-	cout<<endl<<"v3"<<endl;
-	for (unsigned int i=0;i<v1.size();i++) {
-		if (checkPalindrome_v3(v1[i]) == true) {
-			cout << v1[i] << " e' palindroma" << endl;
-		} else {
-			cout << v1[i] << " non e' palindroma" << endl;
-		}
-	}
-	
-	cout<<endl;
-	
-	// BENCHMARK
-	cout<<"Premere invio per iniziare il benchmark...";
-	cin.get();
-	cout<<"BENCH_ITERATIONS = "<<BENCH_ITERATIONS<<endl;
-	*/
-	
-	// BENCHMARK (SENZA FUNZIONE DoTest)
+//	// BENCHMARK (SENZA FUNZIONE DoTest)
 //	bool test1 = true;
 //	bool ritorno1 = false;
 //	int count=0;
@@ -135,6 +137,9 @@ int main (int argc, char **argv) {
 	cout<<"T1: "<<runTimeMillisec_1<<endl;
 	cout<<"T2: "<<runTimeMillisec_2<<endl;
 	cout<<"T3: "<<runTimeMillisec_3<<endl;
+	
+	genera_immagine_grafico(runTimeMillisec_1, runTimeMillisec_2, runTimeMillisec_3);
+	genera_html(runTimeMillisec_1, runTimeMillisec_2, runTimeMillisec_3);
 		
 	return 0;
 }
@@ -211,4 +216,94 @@ int doTest(const vector<string>& toCheck, bool(*checkPalindromeFunz)(const strin
 	cout<<"contatore di prova: "<<count<<endl; // CONTROLLIAMO CHE IL CICLO SIA STATO ESEGUITO
 	count=0;
 	return tempo;
+}
+
+void genera_html(int t1, int t2, int t3) {
+	cout << "Generazione pagina HTML e CSS..." << endl;
+	
+	ofstream pagina;
+	pagina.open("benchmark.html");
+	
+	pagina << "<html>\n";
+	pagina << "<head>\n";
+	pagina << "\t<title>Benchmark</title>\n";
+	pagina << "\t<link href=\"benchmark.css\" rel=\"stylesheet\" type=\"text/css\"></link>\n";
+	pagina << "</head>\n";
+	pagina << "<body>\n";
+	pagina << "\t<h3>BENCH_ITERATIONS = " << BENCH_ITERATIONS << "</h3>\n";
+	pagina << "\t<table>\n";
+	pagina << "\t\t<tr>\n";
+	pagina << "\t\t\t<th>T1</th>\n";
+	pagina << "\t\t\t<th>T2</th>\n";
+	pagina << "\t\t\t<th>T3</th>\n";
+	pagina << "\t\t</tr>\n";
+	pagina << "\t\t<tr>\n";
+	pagina << "\t\t\t<td>" << t1 << "</td>\n";
+	pagina << "\t\t\t<td>" << t2 << "</td>\n";
+	pagina << "\t\t\t<td>" << t3 << "</td>\n";
+	pagina << "\t\t</tr>\n";
+	pagina << "\t</table>\n";
+	pagina << "\t<img src=\"grafico.png\" alt=\"gnuplot\">\n";
+	pagina << "</body>\n";
+	pagina << "</html>\n";
+	
+	pagina.close();
+	
+	ofstream stile;
+	stile.open("benchmark.css");
+	
+	stile << "h3 {\n";
+	stile << "\ttext-align:center\n";
+	stile << "}\n";
+	stile << "table {\n";
+	stile << "\tborder-collapse: collapse;\n";
+	stile << "\ttext-align:center;\n";
+	stile << "\tmargin-left: auto;\n";
+	stile << "\tmargin-right: auto;\n";
+	stile << "}\n";
+	stile << "table, th, td {\n";
+	stile << "\tborder: 1px solid black;\n";
+	stile << "}\n";
+	stile << "th, td {\n";
+	stile << "\tpadding: 15px;\n";
+	stile << "}\n";
+	stile << "img {\n";
+	stile << "\tdisplay: block;\n";
+	stile << "\tmargin: auto;\n";
+	stile << "}\n";
+		
+	stile.close();
+	
+	cout << "Pagina HTML e CSS completata." << endl;
+}
+
+void genera_immagine_grafico (int t1, int t2, int t3) {
+	cout << "Generazione grafico..." << endl;
+	
+	ofstream comandi;
+	comandi.open("comandi_gnuplot.txt");
+	
+	comandi << "set datafile separator \";\"\n";
+	comandi << "set auto x\n";
+	comandi << "set auto y\n";
+	comandi << "set title \'Benchmark\'\n";
+	comandi << 	"set ylabel \'Tempo esec. (millisec)\'\n";
+	comandi << "set xlabel \'Funzioni\'\n";
+	comandi << "set style fill solid\n";
+	comandi << "set term png\n";
+	comandi << "set output \'grafico.png\'\n";
+	comandi << "plot \'benchmark.csv\' using 2:xtic(1) with histogram lt rgb \"red\" notitle\n";
+	
+	ofstream filecsv;
+	filecsv.open("benchmark.csv");
+	
+	filecsv << "1;" << t1 << "\n";
+	filecsv << "2;" << t2 << "\n";
+	filecsv << "3;" << t3 << "\n";
+	
+	filecsv.close();
+	
+	system("\"C:\\Program Files (x86)\\gnuplot\\bin\\gnuplot\" comandi_gnuplot.txt");
+	
+	cout << "Grafico completato. " << endl;
 }
